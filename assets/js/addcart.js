a7d54ca04918f8daa209796656fc30a3856310a0
vaishnavi-2003 <vaishnavi22082003@gmail.com>
2022-01-20 16:14:21 +0530

    added changes

--- assets/js/addcart.js
+++ assets/js/addcart.js
@@ -1,22 +1,28 @@
 let AddAnimal = [];
-        function renderAnimal(addItem) {
-            let leng = addItem.length;
-            console.log(leng);
-            let adding;
-            for (let i = 0; i < leng; i++) {
-                let obj = addItem[i];
-                             
-                let animal = `<div id="dddd">
-                <div id ="img-div" >
+function renderAnimal(addItem) {
+    let leng = addItem.length;
+    console.log(leng);
+    let adding;
+    for (let i = 0; i < leng; i++) {
+        let obj = addItem[i];
+
+        let animal = `<div class="card-container">
+                <div class="card">
+                    <img src=${obj.aurl} class="card-img" alt="imagemissing">
+                    <div class="card-body">
                     <h3>${obj.nanimal}</h3>
-                    <img src=${obj.aurl} id="addpic" alt="imagemissing">
-                    <p><button type="button" id="adoptbt"onclick="clickhandler()">adopt now</button></p>
+                    <button type="button"  onclick="clickhandler()">adopt now</button>
                     </div>
-                    </div>`;
-                adding = adding + animal;
-            }
-            document.getElementById("addingimg").innerHTML = adding;
-        }
-        let getvalue = JSON.parse(localStorage.getItem("value"));
-        renderAnimal(getvalue);
-        console.log(getvalue);
+                    </div>
+                    </div>`
+        adding = adding + animal;
+    }
+    document.getElementById("addingimg").innerHTML = adding;
+}
+let getvalue = JSON.parse(localStorage.getItem("value"));
+renderAnimal(getvalue);
+console.log(getvalue);
+
+function clickhandler(){
+    alert("Thanks for adopting the animal")
+}
