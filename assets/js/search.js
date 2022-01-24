016b5644055e859f2e7a2814e9e36ea57a45be8d
vaishnavi-2003 <vaishnavi22082003@gmail.com>
2022-01-24 14:59:43 +0530

    added changes

---
+++ assets/js/search.js
@@ -0,0 +1,16 @@
+function searchbar() {
+    let searchItem = document.getElementById("search").value;
+    if ("homepage" == searchItem) {
+        window.open("/index.html")
+    }
+    else if ("zoo updates" == searchItem) {
+        window.open("/pages/zooupdate.html")
+    }
+    else if ("register" == searchItem) {
+        window.open("/pages/register.html")
+    }
+    
+    else {
+        alert("page not found")
+    }
+}
